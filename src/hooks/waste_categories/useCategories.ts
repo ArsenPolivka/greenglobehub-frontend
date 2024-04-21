b9d0ccae640f9346cbc4656642c8d5314bcf90ec
PolivkaArsen <arsen.polivka@tetalab.com>
2024-04-21 23:12:26 +0300

    [16]: recycle api fetch implemented

---
+++ src/hooks/waste_categories/useCategories.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getCategories } from "@/api/waste_categories";
+
+type Category = {
+  id: number,
+  name: string,
+}
+
+export const useCategories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        setLoading(true);
+        const result = await getCategories();
+        setCategories(result);
+      } catch (error: any) {
+        console.error(error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return {
+    categories,
+    loading,
+    error,
+  };
+}
